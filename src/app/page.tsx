1d716e4ccc622e4ee1adc489bc93a4823989f031
Dawn Kelly <dawnkelly09@gmail.com>
2024-06-07 14:17:40 -0400

    fix link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ function App() {
       <div className='hero-text-container'>
         <h1>Welcome to Mind Palace 🧠 🏰</h1>
         <h2>Better learning for the rest of us</h2>
-        <Link href="/offerings">Current Offerings</Link>
+        <Link href="/offerings">Click for Current Offerings</Link>
       </div>
 
       <div className='hero-text-body'>
@@ -25,7 +25,7 @@ function App() {
       </div>
       <div className='row-p'>
         <p>If you don't already have one, use the Create Wallet button at the top of the page to create a Smart Wallet. This wallet address will serve as your user profile to interact with gamification and social graph features as they are built.</p>
-        <p>If you want to join our early supporters list, please fill out our <Link href="https://app.deform.cc/form/dcdc8d6b-1d3f-4745-83f9-8fb8783e70c6" target="_blank">Deform</Link></p>
+        <p>If you want to join our early supporters list, please fill out our <Link href="https://app.deform.cc/form/dcdc8d6b-1d3f-4745-83f9-8fb8783e70c6" target="_blank">Deform(click)</Link></p>
         <p>Like and engage with our social media posts on Farcaster and X to help share the project with more builders</p>
       </div>
     </main>
